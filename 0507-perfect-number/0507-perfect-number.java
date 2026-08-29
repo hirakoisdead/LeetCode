16fd4ac23d26ccb13ecf1bba5eb8b83044ecaea1
Tanish <70535268+hirakoisdead@users.noreply.github.com>
2026-08-30 00:54:16 +0530

    Time: 1 ms (95.44%), Space: 42.2 MB (44.91%) - LeetHub

--- 0507-perfect-number/0507-perfect-number.java
+++ 0507-perfect-number/0507-perfect-number.java
@@ -1,14 +1,26 @@
 class Solution {
-    public boolean checkPerfectNumber(int num) { int sum=0; int comp = num;
-        if (num<=1){
+    public boolean checkPerfectNumber(int num) {
+        int sum = 0;
+        int comp = num;
+
+        if (num <= 1) {
             return false;
         }
-        for(int i=1;i<num;i++){
-            if(num%i==0){
-            sum=sum+i;
+
+        for (int i = 1; i * i <= num; i++) {
+            if (num % i == 0) {
+                if (i == num / i) {
+                    sum = sum + i;
+                }
+                else {
+                    sum = sum + i;
+
+                    if (num / i != num) {
+                        sum = sum + (num / i);
+                    }
+                }
             }
         }
-        
-        return (comp==sum);
+        return (comp == sum);
     }
 }
