1b19cdb7c3e8356b3f198347a51499b1e2b3f852
Tanish <70535268+hirakoisdead@users.noreply.github.com>
2026-09-11 23:20:09 +0530

    Time: 17 ms (16.86%), Space: 46.4 MB (41.78%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+
+        HashMap <Character, Integer> freq1=new HashMap<>();
+        HashMap <Character, Integer> freq2=new HashMap<>();
+        
+        for(int i=0; i<s.length(); i++){
+            char c = s.charAt(i);
+
+            freq1.put(c, freq1.getOrDefault(c, 0) + 1);
+
+        }
+        for(int j=0; j<t.length(); j++){
+            char d = t.charAt(j);
+
+            freq2.put(d, freq2.getOrDefault(d, 0) + 1);
+
+        }
+
+        if(freq1.equals(freq2)){
+            return true;
+        }
+        return false;
+
+    }
+}
